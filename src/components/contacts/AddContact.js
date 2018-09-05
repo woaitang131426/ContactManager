272dd210cfb49232ee0587ce3da19ee2c29977ef
woaitang131426 <87383325@qq.com>
2018-09-05 16:36:22 +1200

    ADD_CONTACT, DELETE_CONTACT done

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,36 +1,41 @@
-import React, { Component } from 'react';
-import TextInputGroup from '../layout/TextInputGroup';
+import React, { Component } from "react";
+import TextInputGroup from "../layout/TextInputGroup";
+import { addContact } from "../../actions/contactActions";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import uuid from "uuid";
 
 class AddContact extends Component {
   state = {
-    name: '',
-    email: '',
-    phone: '',
+    name: "",
+    email: "",
+    phone: "",
     errors: {}
   };
 
-  onSubmit = (e) => {
+  onSubmit = e => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
 
     // Check For Errors
-    if (name === '') {
-      this.setState({ errors: { name: 'Name is required' } });
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
       return;
     }
 
-    if (email === '') {
-      this.setState({ errors: { email: 'Email is required' } });
+    if (email === "") {
+      this.setState({ errors: { email: "Email is required" } });
       return;
     }
 
-    if (phone === '') {
-      this.setState({ errors: { phone: 'Phone is required' } });
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is required" } });
       return;
     }
 
     const newContact = {
+      id: uuid(),
       name,
       email,
       phone
@@ -38,15 +43,17 @@ class AddContact extends Component {
 
     //// SUBMIT CONTACT ////
 
+    this.props.addContact(newContact);
+
     // Clear State
     this.setState({
-      name: '',
-      email: '',
-      phone: '',
+      name: "",
+      email: "",
+      phone: "",
       errors: {}
     });
 
-    this.props.history.push('/');
+    this.props.history.push("/");
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -96,4 +103,12 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+AddContact.propTypes = {
+  addContact: PropTypes.func.isRequired
+};
+
+//not bring any data from the global state, so null
+export default connect(
+  null,
+  { addContact }
+)(AddContact);
